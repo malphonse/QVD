05e7eea760323e9bfa6ce2b7f207a2e1e7359d92
malphonse <martin.alphonse@gmail.com>
2020-07-18 23:53:02 -0400

    graph updated to satisfaction

--- T1.R
+++ T1.R
@@ -3,20 +3,17 @@
 library(tidyverse)
 library(RColorBrewer)
 
-?RColorBrewer
 display.brewer.all()
 
-#Convert to tibble
-GC_lac <- as.tibble(OD600_LAC)
+OD600_LAC
 
-GC_lac
-view(GC_lac)
 
 ##Select columns to plot----
  
-plot <- GC_lac %>%
+plot <- OD600_LAC %>%
   select(Time, minQVD, `10ug/mL QVD`, `100ug/ml QVD`, `20mg/kg QVD`)
 
+##View plot
 plot
 
 ##Wrangle (Pivot) to make 2 variable to plot
@@ -25,16 +22,26 @@ graph <- plot %>%
 pivot_longer(c(minQVD, `10ug/mL QVD`, `100ug/ml QVD`, `20mg/kg QVD`), 
              names_to = "condition", values_to = "reading"
              )
+##View graph
+
+graph
+
 #graph ggplot
 
 a <- ggplot(graph, aes(x = Time, y = reading, color = condition))
 
-b <- a + geom_smooth() +
-  facet_wrap(~condition, nrow = 2)
+Figure1 <- a + geom_smooth() +
+  facet_wrap(~condition, nrow = 2) +
+  geom_rug(color = "grey50") +
+  theme_minimal()
 
-c <- b + geom_rug(color = "grey50")
+Figure1A <- a + geom_smooth() +
+  geom_rug(color = "grey50") +
+  theme_minimal()
 
-c + theme_minimal()
+##View Figure
+Figure1
+Figure1A
 
 
 
